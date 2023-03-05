f8ebf07fb54c19827eb4af3279d0e608b97444c7
Musabbir-pr <musabbir313@gmail.com>
2023-03-05 16:12:31 +0600

    added icon and content to the about hero section

--- src/HeroAbout/HeroAbout.js
+++ src/HeroAbout/HeroAbout.js
@@ -1,29 +1,48 @@
 import React from 'react';
+import {AiOutlineCheckCircle} from 'react-icons/ai'
 
 const HeroAbout = () => {
     return (
-        <div className='relative flex mt-20 h-96'>
-            <div className='w-full h-full'>
-                <img src="https://media.istockphoto.com/id/1363734893/photo/happy-family-aboard-a-yacht-out-to-sea.jpg?b=1&s=170667a&w=0&k=20&c=COw-RRsIy5kkeFWM1NALhrEfylEOBkHgliYNuAy14iM=" alt="" />
+        <div className=' flex mt-20  mx-10 '>
+            <div className='w-2/4'>
+                <img className='h-full ' src="https://media.istockphoto.com/id/1363734893/photo/happy-family-aboard-a-yacht-out-to-sea.jpg?b=1&s=170667a&w=0&k=20&c=COw-RRsIy5kkeFWM1NALhrEfylEOBkHgliYNuAy14iM=" alt="" />
             </div>
-            <div className='py-5 px-10 w-/4 bg-gray-300 lg:absolute left-96 top-10 h-96 '>
-                <h6 className='text-xl font-semibold '>About us</h6>
-                <h2 className='text-3xl font-bold'> What we offer</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, modi!</p>
+            <div className='pt-5 pl-12   bg-white shadow-md w-2/4 '>
+                <h6 className='text-xl text-blue-800 font-semibold mb-3'>Why Buy Form Us</h6>
+                <h2 className='text-5xl font-semibold mb-5'> What we offer</h2>
+                <p className='mb-5'>Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Non, modi!</p>
 
                 <div>
-                    <div className='flex'>
-                        <p>icon</p>
+                    <div className='flex  '>
+                       <AiOutlineCheckCircle className='text-4xl mr-5 mt-2 text-blue-600 '></AiOutlineCheckCircle>
                         <div>
-                        <h3 className='font-bold text-xl'>Exiting places</h3>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore quis laborum sed inventore voluptates, odit asperiores voluptate? 
+                        <h3 className='font-bold text-3xl text-blue-600'>Exiting places</h3>
+                        <p>Lorem ipsum dolor sit amet.
                         </p>
                         </div>
                        
                     </div>
-                    <button className='px-5 py-3 bg-orange-600'>
+                    <div className='flex '>
+                       <AiOutlineCheckCircle className='text-4xl mr-5 mt-2 text-blue-600 '></AiOutlineCheckCircle>
+                        <div>
+                        <h3 className='font-bold text-3xl text-blue-600'>Exiting places</h3>
+                        <p>Lorem ipsum dolor sit amet.
+                        </p>
+                        </div>
+                       
+                    </div>
+                    <div className='flex '>
+                       <AiOutlineCheckCircle className='text-4xl mr-5 mt-2 text-blue-600 '></AiOutlineCheckCircle>
+                        <div>
+                        <h3 className='font-bold text-3xl text-blue-600'>Exiting places</h3>
+                        <p>Lorem ipsum dolor sit amet.
+                        </p>
+                        </div>
+                       
+                    </div>
+                    {/* <button className='px-5 py-3 mt-10 bg-blue-500'>
                         Something
-                    </button>
+                    </button> */}
                 </div>
             </div>
             
