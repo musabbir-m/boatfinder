5d12d7e0205bf633d7a1b70ab1b0aff0fb524c48
Musabbir-pr <musabbir313@gmail.com>
2023-03-05 14:01:24 +0600

    edited category and category icons

--- src/Components/Categories/Categories.js
+++ src/Components/Categories/Categories.js
@@ -1,37 +1,68 @@
-import React from 'react';
-import { IoBoatSharp } from 'react-icons/io5';
-import { CgArrowLongRightC } from 'react-icons/cg';
+import React from "react";
+import { IoBoatSharp } from "react-icons/io5";
+import { CgArrowLongRightC } from "react-icons/cg";
+import { GiSailboat } from "react-icons/gi";
+import { TbSailboat } from "react-icons/tb";
 
 const Categories = () => {
-    return (
-        <div className='my-10 py-5 mx-auto bg-gray-200'>
-            <h2 className='text-5xl font-bold text-center'>Boat Categories</h2>
-            <p className='mt-5 text-xl text-gray-700 text-center'>Lorem ipsum dolor sit, amet consectetur <br /> adipisicing elit. Nulla, sint.</p>
-            <div className='py-10 px-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
-                <div className='bg-white max-w-sm   h-72 py-8 px-5 shadow-md border-0  text-center'>
-                    <div className='flex justify-center'><IoBoatSharp className='text-6xl text-blue-500'></IoBoatSharp></div>
-                    <h4 className='text-2xl font-semibold'> Power Boats</h4>
-                    <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, doloribus?</p>
-                    <button className='px-5 py-2 mt-5   border-2  border-orange-200  hover:bg-orange-400 ease-in duration-100' > See all <CgArrowLongRightC className='inline'></CgArrowLongRightC></button>
-
-                </div>
-                <div className='bg-white max-w-sm   h-72 py-8 px-5 shadow-md border-0  text-center'>
-                    <div className='flex justify-center'><IoBoatSharp className='text-6xl text-blue-500'></IoBoatSharp></div>
-                    <h4 className='text-2xl font-semibold'> Power Boats</h4>
-                    <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, doloribus?</p>
-                    <button className='px-5 py-2 mt-5   border-2  border-orange-200  hover:bg-orange-400 ease-in duration-100' > See all <CgArrowLongRightC className='inline'></CgArrowLongRightC></button>
-
-                </div>
-                <div className='bg-white max-w-sm   h-72 py-8 px-5 shadow-md border-0  text-center'>
-                    <div className='flex justify-center'><IoBoatSharp className='text-6xl text-blue-500'></IoBoatSharp></div>
-                    <h4 className='text-2xl font-semibold'> Power Boats</h4>
-                    <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, doloribus?</p>
-                    <button className='px-5 py-2 mt-5   border-2  border-orange-200  hover:bg-orange-400 ease-in duration-100' > See all <CgArrowLongRightC className='inline'></CgArrowLongRightC></button>
-
-                </div>
-            </div>
+  return (
+    <div className="my-10 py-5 mx-auto bg-gray-200">
+      <h2 className="text-5xl font-bold text-center">Boat Categories</h2>
+      <p className="mt-5 text-xl text-gray-700 text-center">
+        Lorem ipsum dolor sit, amet consectetur <br /> adipisicing elit. Nulla,
+        sint.
+      </p>
+      <div className="py-10 px-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+        {/* card1 */}
+        <div className="bg-white max-w-sm   h-72 py-8 px-5 shadow-md border-0  text-center">
+          <div className="flex justify-center">
+            <IoBoatSharp className="text-6xl text-blue-500"></IoBoatSharp>
+          </div>
+          <h4 className="text-2xl font-semibold"> Power Boats</h4>
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
+            doloribus?
+          </p>
+          <button className="px-5 py-2 mt-5   border-2  border-orange-200  hover:bg-orange-400 ease-in duration-100">
+            {" "}
+            See all <CgArrowLongRightC className="inline"></CgArrowLongRightC>
+          </button>
         </div>
-    );
+
+        {/* card-2 */}
+        <div className="bg-white max-w-sm   h-72 py-8 px-5 shadow-md border-0  text-center">
+          <div className="flex justify-center">
+            <GiSailboat className="text-blue-500 text-6xl"></GiSailboat>
+          </div>
+          <h4 className="text-2xl font-semibold"> Sailboats</h4>
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
+            doloribus?
+          </p>
+          <button className="px-5 py-2 mt-5   border-2  border-orange-200  hover:bg-orange-400 ease-in duration-100">
+            {" "}
+            See all <CgArrowLongRightC className="inline"></CgArrowLongRightC>
+          </button>
+        </div>
+
+        {/* Card-3 */}
+        <div className="bg-white max-w-sm   h-72 py-8 px-5 shadow-md border-0  text-center">
+          <div className="flex justify-center">
+            <TbSailboat className="text-6xl text-blue-500"></TbSailboat>
+          </div>
+          <h4 className="text-2xl font-semibold"> Unpowerd Boats</h4>
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
+            doloribus?
+          </p>
+          <button className="px-5 py-2 mt-5   border-2  border-orange-200  hover:bg-orange-400 ease-in duration-100">
+            {" "}
+            See all <CgArrowLongRightC className="inline"></CgArrowLongRightC>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Categories;
