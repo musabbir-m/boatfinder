6250305b82a91dfeaf13ec1dc951a80392688433
Musabbir-pr <musabbir313@gmail.com>
2023-03-08 16:33:45 +0600

    added advertisement option for my products

--- src/Components/MyProducts/MyProducts.js
+++ src/Components/MyProducts/MyProducts.js
@@ -1,10 +1,13 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../Context/AuthProvider";
 import { useQuery } from "@tanstack/react-query";
 import { async } from "@firebase/util";
+import { toast } from "react-hot-toast";
 
 const MyProducts = () => {
   const { user } = useContext(AuthContext);
+
+  const [adbtnLoading, setAddbtnLoading] = useState(false);
   const url = `http://localhost:5000/myproducts?email=${user?.email}`;
   console.log(url);
 
@@ -21,14 +24,37 @@ const MyProducts = () => {
     },
   });
 
+  //advertise product
+  const advertiseItem = (product) => {
+    setAddbtnLoading(true);
+
+    const advertise = { advertised: "true" };
+
+    fetch(`http://localhost:5000/advertise/${product._id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(advertise),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+       toast('advertised successfully')
+        refetch()
+        setAddbtnLoading(false)
+      });
+
+  };
+
   if (isLoading) {
-    return <h2>Loading..</h2>;
+    return <button className="btn btn-square loading"></button>;
   }
   return (
-    <div className="overflow-x-auto">
+    <div className="overflow-x-auto px-5 py-10">
       <h2 className="mb-5 lg:mb-10 text-center text-4xl font-semibold">
         Products Added By You.
       </h2>
+      {adbtnLoading && <button className="btn btn-square loading"></button>}
       <table className="table table-zebra">
         <thead>
           <tr>
@@ -46,8 +72,12 @@ const MyProducts = () => {
               <td>{product.productName}</td>
               <td>{product.price}</td>
               <td>
-                <button className="text-white bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-                  {product.advertised === "false" ? "advertise" : "advertised"}
+                <button disabled
+                  onClick={()=>{advertiseItem(product)}}
+                  className={`text-white bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 `}
+                >
+                  {product.advertised === "false" ? "advertise" : "advertised"}{" "}
+                  
                 </button>
               </td>
               <td>
