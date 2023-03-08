5fa9863f52e83f2f228ced8b328dcfb31073f67d
Musabbir-pr <musabbir313@gmail.com>
2023-03-08 21:10:00 +0600

    added verify and delete buyer funciton

--- src/Components/AllBuyer/AllBuyer.js
+++ src/Components/AllBuyer/AllBuyer.js
@@ -1,14 +1,62 @@
 import React, { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { async } from '@firebase/util';
+import { toast } from 'react-hot-toast';
 
 const AllBuyer = () => {
-    const [seller, setBuyer]= useState([])
-    useEffect(
-    ()=> {
-        fetch('http://localhost:5000/buyer')
-        .then(res=> res.json())
-        .then(data=> setBuyer(data))
-    }, []
-   )
+//     const [buyer, setBuyer]= useState([])
+//     useEffect(
+//     ()=> {
+//         fetch('http://localhost:5000/buyer')
+//         .then(res=> res.json())
+//         .then(data=> setBuyer(data))
+//     }, []
+//    )
+const url= 'http://localhost:5000/buyer'
+const {data:buyers=[], isLoading, refetch}= useQuery({
+    queryKey: ['buyer'],
+    queryFn: async ()=> {
+        const res= await fetch(url)
+        const data= res.json()
+        return data
+
+    }
+
+})
+   //veryfy buyer 
+   const verifyBuyer = (buyer) => {
+   
+
+    const verify = { verified: "true" };
+
+    fetch(`http://localhost:5000/buyer/${buyer._id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(verify),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+       toast('Buyer verified')
+       refetch()
+       
+      });
+  };
+
+
+  //delete buyer 
+  const deleteBuyer= (buyer)=> {
+    const proceed = window.confirm()
+    fetch(`http://localhost:5000/buyer/${buyer._id}`, {
+        method: "DELETE"
+    })
+    .then(res=> res.json())
+    .then(data=> {
+        toast('Deleted Buyer')
+        refetch()
+    })
+  }
     return (
         <div className="overflow-x-auto">
 
@@ -24,26 +72,24 @@ const AllBuyer = () => {
             </tr>
           </thead>
           <tbody>
-            {seller?.map((seller, i) => (
-              <tr key={seller._id}>
+            {buyers?.map((buyer, i) => (
+              <tr key={buyer._id}>
                 <th>{i + 1}</th>
-                <td>{seller.name}</td>
-                <td>{seller.email}</td>
+                <td>{buyer.name}</td>
+                <td>{buyer.email}</td>
                 <td>
-                  <button
+                  <button onClick={()=>{verifyBuyer(buyer)}}
                     type="button"
-                    class="text-white bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                    className="text-white bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
                   >
-                    Verify
+                    {buyer?.verified==='true'? 'verified': 'verify' }
                   </button>
                 </td>
                 <td>
                   <button
-                    onClick={() => {
-                    ;
-                    }}
+                    onClick={() => { deleteBuyer(buyer)  }}
                     type="button"
-                    class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                    className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
                   >
                     Delete
                   </button>
