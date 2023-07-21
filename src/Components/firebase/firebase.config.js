a07fea79e8017653f60bf2af87944e95a75fbd41
musabbir-m <musabbir313@gmail.com>
2023-07-21 23:37:21 +0600

    modified firebase config file

--- src/Components/firebase/firebase.config.js
+++ src/Components/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDLJJOVCJ3bOJokVIJVS0-870Azm0ZLE2o",
-  authDomain: "boatfinder-23f1e.firebaseapp.com",
-  projectId: "boatfinder-23f1e",
-  storageBucket: "boatfinder-23f1e.appspot.com",
-  messagingSenderId: "729897733054",
-  appId: "1:729897733054:web:72bbb407e97d3ef80c07ef"
+  apiKey:process.env.REACT_APP_API_KEY,
+  authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+  projectId:process.env.REACT_APP_PROJECT_ID,
+  storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId:process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
