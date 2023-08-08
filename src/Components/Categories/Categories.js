ff48f41c36eeda86dceb32c5c536ac6412c6dff7
musabbir-m <musabbir313@gmail.com>
2023-08-09 02:01:30 +0600

    added animation to the hero and category seciton

--- src/Components/Categories/Categories.js
+++ src/Components/Categories/Categories.js
@@ -4,6 +4,7 @@ import { CgArrowLongRightC } from "react-icons/cg";
 import { GiSailboat } from "react-icons/gi";
 import { TbSailboat } from "react-icons/tb";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Categories = () => {
   const [categories, setCategories] = useState([]);
@@ -20,15 +21,25 @@ const Categories = () => {
   }
 
   return (
-    <div className=" py-16 mx-auto bg-gray-200">
+    <motion.div
+     className=" py-16 mx-auto bg-gray-200"
+     whileInView={{opacity:[0,1]}}
+     transition={{duration:1.5}}
+
+     >
       <h2 className="md:pt-5 text-3xl  md:text-4xl  text-blue-700  text-center">Boat Categories</h2>
       <p className="mt-5 text-2xl text-gray-700 text-center font-extralight">
         Lorem ipsum dolor sit, amet consectetur <br /> adipisicing elit. Nulla,
         sint.
       </p>
-      <div className="py-10 px-5 flex flex-col justify-center items-center lg:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+      
+      <motion.div 
+      className="py-10 px-5 flex flex-col justify-center items-center lg:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
         {categories?.map((category) => (
-          <div className="bg-white border-b-4 border-blue-500 max-w-sm   h-72 py-8 px-5 shadow-md border-0  text-center">
+          <motion.div 
+          whileHover={{scale:1.1}}
+          transition={{duration:0.5, ease:'easeInOut'}}
+          className="bg-white border-b-4 border-blue-500 max-w-sm   h-72 py-8 px-5 shadow-md border-0  text-center">
             <div className="flex justify-center">
               <IoBoatSharp className="text-6xl text-blue-500"></IoBoatSharp>
             </div>
@@ -46,10 +57,10 @@ const Categories = () => {
             >
               See all <CgArrowLongRightC className="inline"></CgArrowLongRightC>
             </Link>
-          </div>
+          </motion.div>
         ))}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
